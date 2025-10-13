8e961d6a570fe32e755d257f80d98c99b010950c
sysmkr <to.sysmkr@gmail.com>
2025-10-13 15:53:28 +0200

    [main00] raw

---
+++ init.lua
@@ -0,0 +1,34 @@
+-- bootstrap Lazy.nvim (if not already done)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- enable true colors
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+-- plugin setup
+require("lazy").setup({
+    {
+        "Shatur/neovim-ayu",
+        config = function()
+            require("ayu").setup({
+                mirage = false,
+                overrides = {},
+            })
+            vim.cmd("colorscheme ayu")
+        end,
+    },
+})
